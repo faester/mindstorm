363f7539f3f3c2075d9d8f69585c3975cbcdf09b
Morten Fæster <morten.faester@jppol.dk>
2019-07-08 22:35:19 +0200

    Adding a template.

--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index(): 
 	return 'Index!?'
 
-@app.route('/
+@app.route('/test-template')
+def testTemplate(): 
+	return render_template('test.html', title = "Testing templates", greeting = "Hello Bandut")
 
