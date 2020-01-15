b86124171a9ac303df0d5ec16451654a417a703b
Morten Fæster <morten.faester@jppol.dk>
2020-01-15 23:20:18 +0100

    First meaningful test

--- server/tests/test_TachoMotor.py
+++ server/tests/test_TachoMotor.py
@@ -6,6 +6,6 @@ import flaskr.mindstorm.TachoMotor
 def test_ctor():
 	subject = flaskr.mindstorm.TachoMotor.TachoMotor("../../sys/class", 0)
 	actual = subject.commands()
-	assert all([a == b for a, b in zip(actual, ['run-forever', 'run-to-abs-pos', 'run-to-rel-pos', 'run-timed', 'run-direct', 'stop', 'reset'])])
-
-	
+	expected = ['run-forever', 'run-to-abs-pos', 'run-to-rel-pos', 'run-timed', 'run-direct', 'stop', 'reset']
+	assert all([a == b for a, b in zip(actual, expected)])
+	assert len(actual) == len(expected), "number of elements differ"
