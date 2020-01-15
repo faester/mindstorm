22ae35b50810f02ec2c9b728607604b8abb27759
Morten Fæster <morten.faester@jppol.dk>
2020-01-15 23:43:54 +0100

    Simple tests - reading a file in Python

--- server/flaskr/mindstorm/TachoMotor.py
+++ server/flaskr/mindstorm/TachoMotor.py
@@ -1,10 +1,17 @@
+import os
+
 class TachoMotor:
 	""" Abstraction of a motor in Mindstorms on EV3 """
-
 
 	def __init__(self, basedir, motorNumber): 
 		self.basedir = basedir
 		self.motorNumber = motorNumber
+		self.directory = os.path.join(basedir, "tacho-motor", f'motor{motorNumber}')
+		self.commandsArray = self.commands()
 
 	def commands(self): 
-		return ["a","b","c"]		
+		commands = open(os.path.join(self.directory, "commands"), "r")
+		contents = commands.read().replace('\n', '')
+		commands.close()
+		print(contents.split(' '))
+		return contents.split(' ')
--- server/tests/test_TachoMotor.py
+++ server/tests/test_TachoMotor.py
@@ -2,9 +2,23 @@ import pytest
 
 import flaskr
 import flaskr.mindstorm.TachoMotor
+import os
 
-def test_ctor():
-	subject = flaskr.mindstorm.TachoMotor.TachoMotor("../../sys/class", 0)
+def motorPath(): 
+	return os.path.join('..', 'sys', 'class')
+
+def test_ctor_when_invalid_motor():
+	with pytest.raises(FileNotFoundError) as exception:
+		subject = flaskr.mindstorm.TachoMotor.TachoMotor(motorPath(), 10)
+
+def test_ctor_when_valid_motor():
+	try:
+		subject = flaskr.mindstorm.TachoMotor.TachoMotor(motorPath(), 0)
+	except FileNotFoundError:
+		pytest.fail('Did not expect an error opening motor0')
+
+def test_commands():
+	subject = flaskr.mindstorm.TachoMotor.TachoMotor(motorPath(), 0)
 	actual = subject.commands()
 	expected = ['run-forever', 'run-to-abs-pos', 'run-to-rel-pos', 'run-timed', 'run-direct', 'stop', 'reset']
 	assert all([a == b for a, b in zip(actual, expected)])
