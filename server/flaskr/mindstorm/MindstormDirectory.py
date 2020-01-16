c9b5d21a8ea51c43cbbd3b5b25dd7878c3bb817a
Morten Fæster <morten.faester@jppol.dk>
2020-01-16 23:24:45 +0100

    File logic in separate class

---
+++ server/flaskr/mindstorm/MindstormDirectory.py
@@ -0,0 +1,22 @@
+import os
+
+class MindstormDirectory:
+	""" Defines methods for communicating with a mindstorm directory """
+
+	def __init__(self, directory):
+		self.directory = directory
+	
+	def __openFileInSubdir(self, filename, mode): 
+		return open(os.path.join(self.directory, filename), mode)
+
+	def readFromFile(self, filename):
+		f = self.__openFileInSubdir(filename, "r")
+		content = f.read().replace('\n', '')
+		f.close()
+		return content
+
+	def writeToFile(self, filename, content):
+		f = self.__openFileInSubdir(filename, "w")
+		f.write(content)
+		f.close()	
+
--- server/flaskr/mindstorm/TachoMotor.py
+++ server/flaskr/mindstorm/TachoMotor.py
@@ -1,34 +1,19 @@
 import os
+import flaskr.mindstorm.MindstormDirectory
 
 class TachoMotor:
 	""" Abstraction of a motor in Mindstorms on EV3 """
 
 	def __init__(self, basedir, motorNumber): 
-		self.basedir = basedir
-		self.motorNumber = motorNumber
 		self.directory = os.path.join(basedir, "tacho-motor", f'motor{motorNumber}')
+		self.mindstormDirectory = flaskr.mindstorm.MindstormDirectory.MindstormDirectory(self.directory)
 		self.commandsArray = self.commands()
 
-	def __openFileInSubdir(self, filename, mode): 
-		return open(os.path.join(self.directory, filename), mode)
-
-	def __readFromSubdir(self, filename):
-		f = self.__openFileInSubdir(filename, "r")
-		content = f.read().replace('\n', '')
-		f.close()
-		return content
-
-	def __writeToSubdir(self, filename, content):
-		f = self.__openFileInSubdir(filename, "w")
-		f.write(content)
-		f.close()
-		
-
 	def commands(self): 
-		return self.__readFromSubdir("commands").split(' ')
+		return self.mindstormDirectory.readFromFile("commands").split(' ')
 
 	def sendCommand(self, command):
-		self.__writeToSubdir("command", command)
+		self.mindstormDirectory.writeToFile("command", command)
 
 	def setSpeed(self, speed): 
-		commandFile = self.__writeToSubdir("speed_sp", f'{speed}\n')
+		self.mindstormDirectory.writeToFile("speed_sp", f'{speed}\n')
