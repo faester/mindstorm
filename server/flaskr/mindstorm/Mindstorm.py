793f1893da03494296412b25e4f60c6ceb3d58a5
Morten Fæster <morten.faester@jppol.dk>
2020-01-23 01:33:45 +0100

    Moved SensorMotorIO to Mindstorm.py

--- server/flaskr/mindstorm/Mindstorm.py
+++ server/flaskr/mindstorm/Mindstorm.py
@@ -26,3 +26,54 @@ class Directory:
 
 	def subdir(self, subdir):
 		return os.path.join(self.directory, subdir)
+
+class SensorMotorIO:
+	""" Performs conversions and mappings to/from mindstorm dir """
+	def __init__(self, mindstormDirectory):
+		self.mindstormDirectory = mindstormDirectory
+		self.readable_keys = []
+		self.writable_keys = []
+		self.mappers = {}
+		def dosplit(x): return [item.replace('\n', '') for item in x.split(' ')]
+		def str_no_newline(x): return x.replace('\n', '')
+		def dictionary(x): 
+			result={}
+			for s in x.split('\n'):
+				if '=' in s: 
+					k, v = s.split('=')
+					result[k] = v.replace('\n', '')
+			return result
+		self.__split__ = dosplit
+		self.__str__ = str_no_newline
+		self.__dictionary__ = dictionary
+
+	def __add_key__(self, key, mode):
+		if 'w' in mode: self.writable_keys.append(key)
+		if 'r' in mode: self.readable_keys.append(key)
+
+	def add_int_file(self, file_name, mode):
+		self.mappers[file_name] = int
+		self.__add_key__(file_name, mode)
+
+	def add_string_file(self, file_name, mode):
+		self.mappers[file_name] = self.__str__
+		self.__add_key__(file_name, mode)
+
+	def add_dictionary_file(self, file_name, mode):
+		self.mappers[file_name] = self.__dictionary__
+		self.__add_key__(file_name, mode)
+
+	def add_array_file(self, file_name, mode):
+		self.mappers[file_name] = self.__split__
+		self.__add_key__(file_name, mode)
+	
+	def get(self):
+		result = {}
+		for file_name in self.readable_keys:
+			result[file_name] = self.mappers[file_name](self.mindstormDirectory.read_from_file(file_name))
+		return result
+	
+	def post(self, **kwargs):
+		for file_name in [f for f in self.writable_keys if f in kwargs]:
+			self.mindstormDirectory.write_to_file(file_name, str(kwargs[file_name]))
+
--- server/flaskr/mindstorm/TachoMotor.py
+++ server/flaskr/mindstorm/TachoMotor.py
@@ -1,56 +1,5 @@
 import flaskr.mindstorm.Mindstorm as Mindstorm 
 
-class SensorMotorIO:
-	""" Performs conversions and mappings to/from mindstorm dir """
-	def __init__(self, mindstormDirectory):
-		self.mindstormDirectory = mindstormDirectory
-		self.readable_keys = []
-		self.writable_keys = []
-		self.mappers = {}
-		def dosplit(x): return [item.replace('\n', '') for item in x.split(' ')]
-		def str_no_newline(x): return x.replace('\n', '')
-		def dictionary(x): 
-			result={}
-			for s in x.split('\n'):
-				if '=' in s: 
-					k, v = s.split('=')
-					result[k] = v.replace('\n', '')
-			return result
-		self.__split__ = dosplit
-		self.__str__ = str_no_newline
-		self.__dictionary__ = dictionary
-
-	def __add_key__(self, key, mode):
-		if 'w' in mode: self.writable_keys.append(key)
-		if 'r' in mode: self.readable_keys.append(key)
-
-	def add_int_file(self, file_name, mode):
-		self.mappers[file_name] = int
-		self.__add_key__(file_name, mode)
-
-	def add_string_file(self, file_name, mode):
-		self.mappers[file_name] = self.__str__
-		self.__add_key__(file_name, mode)
-
-	def add_dictionary_file(self, file_name, mode):
-		self.mappers[file_name] = self.__dictionary__
-		self.__add_key__(file_name, mode)
-
-	def add_array_file(self, file_name, mode):
-		self.mappers[file_name] = self.__split__
-		self.__add_key__(file_name, mode)
-	
-	def get(self):
-		result = {}
-		for file_name in self.readable_keys:
-			result[file_name] = self.mappers[file_name](self.mindstormDirectory.read_from_file(file_name))
-		return result
-	
-	def post(self, **kwargs):
-		for file_name in [f for f in self.writable_keys if f in kwargs]:
-			self.mindstormDirectory.write_to_file(file_name, str(kwargs[file_name]))
-
-
 class Motor:
 	""" Abstraction of a motor in Mindstorms on EV3 """
 	def __init__(self, basedir, motorNumber = None): 
@@ -58,7 +7,7 @@ class Motor:
 			self.mindstormDirectory = Mindstorm.Directory(basedir)
 		else:
 			self.mindstormDirectory = Mindstorm.Directory(basedir, "tacho-motor", f'motor{motorNumber}')
-		self.motorIO = SensorMotorIO(self.mindstormDirectory)
+		self.motorIO = Mindstorm.SensorMotorIO(self.mindstormDirectory)
 		self.__construct_metadata__()
 		self.commands()
 	
