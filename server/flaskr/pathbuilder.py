ca003a99bcb285f506ba1ae3bca1b2bc03d0a577
Morten Fæster <morten.faester@jppol.dk>
2020-01-24 00:21:16 +0100

    Very simple template for motor

--- server/flaskr/pathbuilder.py
+++ server/flaskr/pathbuilder.py
@@ -1,6 +1,6 @@
 import logging
 import flaskr.mindstorm.TachoMotor as TachoMotor
-from flask import jsonify, request
+from flask import jsonify, request, render_template
 import json
 
 def with_logging(func):
@@ -16,6 +16,32 @@ def with_logging(func):
 			raise ex
 	return logit
 
+class Decoder: 
+	def __init__(self):
+		self.log = logging.getLogger('decode')
+
+	def decode(self, req):
+		self.log.info('is decoding')
+		contentType = req.headers.get('Content-Type')
+		self.log.info(contentType)
+		if contentType == 'application/x-www-form-urlencoded': 
+			self.log.info('form encoding')
+			return req.form
+		return req.json
+	
+class Encoder: 
+	def __init__(self):
+		self.log = logging.getLogger('Encoder')
+
+	def encode(self, req, template_name, response_data):
+		acceptHeader = req.headers.get('Accept')
+		if 'text/html' in acceptHeader.split(','):
+			self.log.debug(f'Got "{acceptHeader}" accept reader. Trying the template {template_name}')
+			return render_template(template_name, **response_data)
+		self.log.debug(f'Got "{acceptHeader}" accept reader. Returning response data.')
+		return response_data
+
+
 class PathBuilder:
 	@with_logging
 	def __init__(self, app, basedir):
@@ -23,31 +49,25 @@ class PathBuilder:
 		self.app = app
 		self.logger.debug('initialized') 
 		self.basedir = basedir
+		self.decoder = Decoder()
+		self.encoder = Encoder()
 		self.__setup_routes__()
 
 	def __setup_routes__(self): 
-		def decode(req):
-			log = logging.getLogger('decode')
-			log.info('is decoding')
-			contentType = req.headers.get('Content-Type')
-			log.info(contentType)
-			if contentType == 'application/x-www-form-urlencoded': 
-				log.info('form encoding')
-				return req.form
-			return req.json
 
 		@self.app.route('/motors')
 		def motors():
 			motorList = TachoMotor.MotorList(self.basedir) 
-			return motorList.get_motor_list()
+			return self.encoder.encode(request, 'motors.html', {"motors": motorList.get_motor_list()})
 
 		@self.app.route('/motors/<motor_name>')
 		def get_motor(motor_name):
 			motor = TachoMotor.Motor(self.basedir, motor_name = motor_name)
-			return motor.get()
+			return self.encoder.encode(request, 'motor.html', motor.get())
 
 		@self.app.route('/motors/<motor_name>', methods = ['POST'])
 		def post_motor(motor_name):
 			motor = TachoMotor.Motor(self.basedir, motor_name = motor_name)
-			body = decode(request)
-			return motor.post(**body)
+			body = self.decoder.decode(request)
+			motor.post(**body)
+			return self.encoder.encode(request, 'motor.html', motor.get())
