30866ba14017f356206ed25f5f24b9fa7448a0f2
Morten Faester <faester@gmail.com>
2020-10-14 23:54:46 +0000

    Don't create debug output.

--- hangplot/where-are-we-now.sh
+++ hangplot/where-are-we-now.sh
@@ -6,11 +6,7 @@ rightpos=$(cat $right/position)
 
 r1=$(awk "BEGIN {print $standardLength - $leftpos/$degreesPerMilli}")
 r2=$(awk "BEGIN {print $standardLength - $rightpos/$degreesPerMilli}")
-
-echo $leftpos $r1 $rightpos $r2
-
 x=$(awk "BEGIN {print int(($r1 - $r2 + $width^2)/(2 * $width))}")
-echo finding y
 y=$(awk "BEGIN {print int(sqrt($r1 * $r1 - $x * $x))}")
 
 echo $x $y
