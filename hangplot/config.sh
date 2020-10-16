1ce9951e92a9b0b2fa030de5facc1efe8a0bd0a8
Morten Faester <faester@gmail.com>
2020-10-16 07:21:44 +0000

    Better moves

--- hangplot/config.sh
+++ hangplot/config.sh
@@ -1,7 +1,7 @@
-# Created using init.sh it Wed Oct 14 23:29:19 UTC 2020
+# Created using init.sh it Fri Oct 16 06:29:33 UTC 2020
 left=/sys/class/tacho-motor/motor0
 right=/sys/class/tacho-motor/motor1
 pen=/sys/class/tacho-motor/motor2
 degreesPerMilli=5.76
-width=425
+width=430
 standardLength=500
--- hangplot/move-to.sh
+++ hangplot/move-to.sh
@@ -14,21 +14,39 @@ fi
 wait_for_motor() {
 	while [ $(cat $1/state ) ]
 	do
+		echo Waiting for $1
 		cat $1/state
 		sleep 1
 	done
 }
 
+current_length() {
+	position=$(cat $1/position)
+	length=$(awk "BEGIN {print int($standardLength-$position*$degreesPerMilli)}")
+	echo $length
+}
+
 pos=($(./where-are-we-now.sh))
-deltax=$(awk "BEGIN {print int(($targetx - ${pos[0]}) * $degreesPerMilli)}")
-deltay=$(awk "BEGIN {print int(($targety - ${pos[1]}) * $degreesPerMilli)}")
 
-speedX=100
-speedY=$(awk "BEGIN {print int($speedX*($deltay/$deltax))}")
-echo $deltax $deltay $speedX $speedY
+let oppositeX=$width-$targety
+desiredLengthLeft=$(awk "BEGIN {print int(sqrt($targetx*$targetx + $targety*$targety )) } ")
+desiredLengthRight=$(awk "BEGIN {print int(sqrt($oppositeX*$oppositeX + $targety*$targety )) } ")
 
-./move-rel.sh l $deltax $speedX
-./move-rel.sh r $deltay $speedY
+let deltaleft=$desiredLengthLeft-$(current_length $left)
+let deltaright=$desiredLengthRight-$(current_length $right)
+
+degreesleft=$(awk "BEGIN {print int($deltaleft * $degreesPerMilli)}")
+degreesright=$(awk "BEGIN {print int($deltaright * $degreesPerMilli)}")
+
+speedleft=100
+speedright=$(awk "BEGIN {print int($speedleft * ($deltaright / $deltaleft))}")
+
+echo $desiredLengthLeft $desiredLengthRight
+echo $deltaleft $deltaright
+echo $degreesleft $degreesright $speedleft $speedright
+
+./move-rel.sh l $degreesleft $speedleft
+./move-rel.sh r $degreesright $speedright
 
 wait_for_motor $left
 wait_for_motor $right
